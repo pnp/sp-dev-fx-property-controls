4ded724e5718bf18ab10223b9e181b4fc0138099
Elio Struyf <info@estruyf.be>
2017-12-08 17:02:01 +0100

    Termpicker updated with the ability to scope on group or termset

--- src/webparts/propertyControlsTest/PropertyControlsTestWebPart.ts
+++ src/webparts/propertyControlsTest/PropertyControlsTestWebPart.ts
@@ -137,6 +137,8 @@ export default class PropertyControlsTestWebPart extends BaseClientSideWebPart<I
                   disabled: false,
                   onGetErrorMessage: null,
                   deferredValidationTime: 0,
+                  limitByGroupNameOrID: 'People',
+                  limitByTermsetNameOrID: 'Location',
                   key: 'termSetsPickerFieldId'
                 }),
                 PropertyFieldDateTimePicker('datetime', {

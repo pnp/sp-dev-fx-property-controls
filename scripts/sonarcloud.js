1f82d3888c20b0fae13de91707ae80d7d2a6ba95
Elio Struyf <info@estruyf.be>
2018-09-17 14:41:09 +0200

    SonarCloud updates

--- scripts/sonarcloud.js
+++ scripts/sonarcloud.js
@@ -11,7 +11,7 @@ const package = fs.readFileSync(path.join(__dirname, "../package.json"), "UTF8")
 let sonarProps = fs.readFileSync(path.join(__dirname, "../sonar-project.properties"), "UTF8");
 if (package && sonarProps) {
   const pkg = JSON.parse(package);
-  sonarProps += `sonar.login==${token}\n`;
+  sonarProps += `sonar.login=${token}\n`;
   sonarProps += `sonar.projectVersion=${pkg.version}\n`;
   fs.writeFileSync(path.join(__dirname, "../sonar-project.properties"), sonarProps, { encoding: "UTF8" })
 }

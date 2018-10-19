eb2c6dc7a45b0fead72cf1c08bc57082f152d5d4
Richard Gigan <delpoolp@gmail.com>
2018-10-19 11:53:26 +0200

    French translation

---
+++ src/loc/fr-fr.js
@@ -0,0 +1,84 @@
+define([], function () {
+  return {
+    propertyFieldMultiSelectNoOptions: "Aucune option à sélectionner",
+    InvalidUrlError: "L'URL fournie n'est pas valide",
+    // Common field labels
+    'SaveButtonLabel': 'Sauvegarder',
+    'CancelButtonLabel': 'Annuler',
+
+    // PeoplePicker labels
+    'PeoplePickerSuggestedContacts': 'Personnes suggérées',
+    'PeoplePickerSuggestedGroups': 'Groupes suggérés',
+    'PeoplePickerSuggestedCombined': 'Personnes et groupes suggérés',
+    'PeoplePickerNoResults': 'Aucun résultat trouvé',
+    'PeoplePickerLoading': 'Chargement des résultats ...',
+
+    // DatePicker labels
+    'DatePickerMonthLongJanuary': 'Janvier',
+    'DatePickerMonthShortJanuary': 'Jan.',
+    'DatePickerMonthLongFebruary': 'Février',
+    'DatePickerMonthShortFebruary': 'Fev.',
+    'DatePickerMonthLongMarch': 'Mars',
+    'DatePickerMonthShortMarch': 'Mar.',
+    'DatePickerMonthLongApril': 'Avril',
+    'DatePickerMonthShortApril': 'Avr.',
+    'DatePickerMonthLongMay': 'Mai',
+    'DatePickerMonthShortMay': 'Mai',
+    'DatePickerMonthLongJune': 'Juin',
+    'DatePickerMonthShortJune': 'Juin',
+    'DatePickerMonthLongJuly': 'Juillet',
+    'DatePickerMonthShortJuly': 'Jul.',
+    'DatePickerMonthLongAugust': 'Août',
+    'DatePickerMonthShortAugust': 'Août',
+    'DatePickerMonthLongSeptember': 'Septembre',
+    'DatePickerMonthShortSeptember': 'Sept.',
+    'DatePickerMonthLongOctober': 'Octobre',
+    'DatePickerMonthShortOctober': 'Oct.',
+    'DatePickerMonthLongNovember': 'Novembre',
+    'DatePickerMonthShortNovember': 'Nov.',
+    'DatePickerMonthLongDecember': 'Decembre',
+    'DatePickerMonthShortDecember': 'Dec.',
+    'DatePickerDayLongSunday': 'Dimanche',
+    'DatePickerDayShortSunday': 'Dim',
+    'DatePickerDayLongMonday': 'Lundi',
+    'DatePickerDayShortMonday': 'Lun',
+    'DatePickerDayLongTuesday': 'Mardi',
+    'DatePickerDayShortTuesday': 'Mar',
+    'DatePickerDayLongWednesday': 'Mercredi',
+    'DatePickerDayShortWednesday': 'Mer',
+    'DatePickerDayLongThursday': 'Jeudi',
+    'DatePickerDayShortThursday': 'Jeu',
+    'DatePickerDayLongFriday': 'Vendredi',
+    'DatePickerDayShortFriday': 'Ven',
+    'DatePickerDayLongSaturday': 'Samedi',
+    'DatePickerDayShortSaturday': 'Sam',
+
+    'DatepickerGoToToday': 'Aujourd\'hui',
+    'DateTimePickerDate': 'Date',
+    'DateTimePickerTime': 'Temps',
+
+    // ColorPicker Labels
+    'ColorPickerButtonTitle': 'Choisis une couleur',
+
+    // Number field validation messages
+    'NotNumberValidationMessage': 'La valeur doit être un nombre, la valeur actuel est :',
+    'MinimumNumberValidationMessage': 'La valeur doit être supérieure à :',
+    'MaximumNumberValidationMessage': 'La valeur doit être inférieure à :',
+
+    // TermPicker
+    "TermPickerNoTerms": "L'ensemble de termes ne contient aucun terme",
+    "TermPickerExpandTitle": "Développer cet ensemble de termes",
+    "TermPickerExpandNode": "Développer ce nœud",
+    "TermPickerMenuTermSet": "Menu pour l'ensemble de termes",
+    "TermPickerMenuGroup": "Menu pour l'ensemble de termes",
+    "TermPickerInLabel": "dans",
+    "TermPickerTermSetLabel": "Ensemble de termes",
+
+    // Collection data
+    "CollectionDataEmptyFields": "Aucun champ n'a été fourni pour la collection de données.",
+    "CollectionDataEmptyValue": "Aucune donnée dans votre collection.",
+    'CollectionAddRowButtonLabel': 'Ajouter des données à la collection',
+    'CollectionDeleteRowButtonLabel': 'Supprimer la ligne',
+    'CollectionSaveAndAddButtonLabel': 'Ajouter et sauvegarder',
+  }
+});

48a3ffe887af16a0290b6410cf0542a13a88b382
Elio Struyf <info@estruyf.be>
2020-01-28 17:28:56 +0100

    Added PropertyPaneHelpers

---
+++ src/helpers/PropertyPaneHelpers.ts
@@ -0,0 +1,82 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Spinner, ISpinnerProps } from 'office-ui-fabric-react/lib/Spinner';
+
+export class PropertyPaneHelpers {
+  private static propertyPaneElm: HTMLElement = null;
+  private static spinnerElm: HTMLElement = null;
+
+  /**
+   * Add a spinner for the `loadPropertyPaneResources` method
+   */
+  public static setSpinner(props: ISpinnerProps) {
+    this.clearSpinner();
+    const className = `.spPropertyPaneContainer`;
+
+    this.waitForElement(className).then(propPanelElm => {
+      if (propPanelElm) {
+        this.propertyPaneElm = propPanelElm;
+        const spinnerElm = document.createElement("div");
+        this.spinnerElm = propPanelElm.appendChild(spinnerElm);
+        const element: React.ReactElement<ISpinnerProps> = React.createElement(Spinner, {
+          ...props
+        });
+        ReactDom.render(element, propPanelElm);
+      }
+    });
+  }
+
+  /**
+   * Clear the spinner from the property pane
+   */
+  public static clearSpinner() {
+    // Check if the property pane element exists and remove the styling
+    if (this.propertyPaneElm) {
+      this.propertyPaneElm = null;
+    }
+    if (this.spinnerElm) {
+      ReactDom.unmountComponentAtNode(this.spinnerElm);
+      this.spinnerElm = null;
+    }
+  }
+
+
+  /**
+   * Waiting until an element exists
+   *
+   * @param selector
+   */
+  private static waitForElement(selector): Promise<HTMLElement | null> {
+    return new Promise((resolve, reject) => {
+      var element = document.querySelector(selector);
+
+      if (element) {
+        resolve(element);
+        return;
+      }
+
+      var observer = new MutationObserver((mutations) => {
+        // Timeout
+        const timer = setTimeout(() => {
+          observer.disconnect();
+          resolve(null);
+          return;
+        }, 5000);
+
+        mutations.forEach((mutation) => {
+          var nodes = [].slice.call(mutation.addedNodes);
+          for (var node of nodes) {
+            if (node.matches && node.matches(selector)) {
+              clearTimeout(timer);
+              observer.disconnect();
+              resolve(node);
+              return;
+            }
+          }
+        });
+      });
+
+      observer.observe(document.documentElement, { childList: true, subtree: true });
+    });
+  }
+}

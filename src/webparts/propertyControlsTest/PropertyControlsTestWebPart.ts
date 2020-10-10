c311e46e3145f960325e4520eeac1cf5511a0c5e
Gautam Sheth <gautamdsheth@outlook.com>
2020-10-10 17:15:18 +0530

    Escape single quote + Add support special chars for folder names

--- src/webparts/propertyControlsTest/PropertyControlsTestWebPart.ts
+++ src/webparts/propertyControlsTest/PropertyControlsTestWebPart.ts
@@ -923,8 +923,8 @@ export default class PropertyControlsTestWebPart extends BaseClientSideWebPart<I
                   canCreateFolders: true,
                   onSelect: ((folder: IFolder) => { console.log(folder); this.properties.folderPicker = folder; }),
                   rootFolder: {
-                    Name: "Documents",
-                    ServerRelativeUrl: "/sites/gautamdev/Shared Documents"
+                    Name: "GOBCPPT",
+                    ServerRelativeUrl: "/GOBCPPT"
                   },
                 }),
               ]

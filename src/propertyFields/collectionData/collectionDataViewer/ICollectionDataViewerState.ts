e27659c986ad7b72cd3a871be93ef4b327cd72fb
Magnus Ellinge <magnus.ellinge@consid.se>
2021-11-17 22:45:49 +0100

    Fix merge

--- src/propertyFields/collectionData/collectionDataViewer/ICollectionDataViewerState.ts
+++ src/propertyFields/collectionData/collectionDataViewer/ICollectionDataViewerState.ts
@@ -3,6 +3,6 @@ import { FieldValidator } from "..";
 export interface ICollectionDataViewerState {
   crntItems: any[];
   inCreationItem?: any;
-  inCreationItemValid?: boolean,
+  inCreationItemValid?: boolean;
   validation?: FieldValidator;
 }

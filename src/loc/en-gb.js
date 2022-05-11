3c1c1445bcfb673c7062e455dcb696e4d17de7e3
Peter Cox <pcox@contentformula.com>
2022-05-11 09:05:48 +0100

    Fix translation

--- src/loc/en-gb.js
+++ src/loc/en-gb.js
@@ -161,7 +161,7 @@ define([], function ()
     OneDriveRootFolderName: "Files",
     OpenButtonLabel: "Open",
     PhotoIconUrl: "https://spoprod-a.akamaihd.net/files/odsp-next-prod_2019-01-11_20190116.001/odsp-media/images/itemtypes/20_2x/photo.png",
-    OrgAssetsTabLabel: "Images and files provided by your organization",
+    OrgAssetsTabLabel: "Images and files provided by your organisation",
     PoweredByBing: "Powered by Bing",
     RecentDocumentsHeader: "Recent documents",
     RecentImagesHeader: "Recent images",

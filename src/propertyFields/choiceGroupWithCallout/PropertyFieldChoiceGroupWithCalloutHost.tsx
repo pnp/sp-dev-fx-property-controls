c5b6c9dccbd9232376f82d79b04b894725de31e9
Alex Terentiev <aterentiev@microsoft.com>
2023-12-02 15:28:47 -0500

    fixes #592

--- src/propertyFields/choiceGroupWithCallout/PropertyFieldChoiceGroupWithCalloutHost.tsx
+++ src/propertyFields/choiceGroupWithCallout/PropertyFieldChoiceGroupWithCalloutHost.tsx
@@ -1,28 +1,33 @@
-import * as React from 'react';
-import omit from 'lodash/omit';
+import * as React from "react";
+import omit from "lodash/omit";
 
-import PropertyFieldHeader from '../../common/propertyFieldHeader/PropertyFieldHeader';
-import type { IPlaceholderWithCalloutProps } from '../../common/placeholderWithCallout/IPlaceholderWithCallout';
+import PropertyFieldHeader from "../../common/propertyFieldHeader/PropertyFieldHeader";
+import type { IPlaceholderWithCalloutProps } from "../../common/placeholderWithCallout/IPlaceholderWithCallout";
 
-import { IPropertyFieldChoiceGroupWithCalloutHostProps } from './IPropertyFieldChoiceGroupWithCalloutHost';
-import * as telemetry from '../../common/telemetry';
-import { ChoiceGroup } from '@fluentui/react/lib/components/ChoiceGroup';
+import { IPropertyFieldChoiceGroupWithCalloutHostProps } from "./IPropertyFieldChoiceGroupWithCalloutHost";
+import * as telemetry from "../../common/telemetry";
+import { ChoiceGroup } from "@fluentui/react/lib/components/ChoiceGroup";
 
-export default class PropertyFieldToggleWithCalloutHost extends React.Component<IPropertyFieldChoiceGroupWithCalloutHostProps, null> {
-    constructor(props: IPropertyFieldChoiceGroupWithCalloutHostProps) {
-        super(props);
+export default class PropertyFieldToggleWithCalloutHost extends React.Component<
+  IPropertyFieldChoiceGroupWithCalloutHostProps,
+  null
+> {
+  constructor(props: IPropertyFieldChoiceGroupWithCalloutHostProps) {
+    super(props);
 
-        telemetry.track('PropertyFieldToggleWithCallout', {
-            disabled: props.disabled
-        });
-    }
+    telemetry.track("PropertyFieldToggleWithCallout", {
+      disabled: props.disabled,
+    });
+  }
 
-    public render(): JSX.Element {
-        return (
-            <div>
-                <PropertyFieldHeader {...(this.props as IPlaceholderWithCalloutProps)} />
-                <ChoiceGroup {...omit(this.props, ['label'])} />
-            </div>
-        );
-    }
+  public render(): JSX.Element {
+    return (
+      <div>
+        <PropertyFieldHeader
+          {...(this.props as IPlaceholderWithCalloutProps)}
+        />
+        <ChoiceGroup {...omit(this.props, ["label"])} />
+      </div>
+    );
+  }
 }
